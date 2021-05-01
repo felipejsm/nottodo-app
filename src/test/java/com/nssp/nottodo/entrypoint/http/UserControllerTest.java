1cab6e0eeb5a680efb0c048bbdc18791e57f77ac
felipejsm <felipejsm41@gmail.com>
2021-05-01 16:27:50 -0300

    Remocao de imports inutilizados e trocando MockBean por Autowired

--- src/test/java/com/nssp/nottodo/entrypoint/http/UserControllerTest.java
+++ src/test/java/com/nssp/nottodo/entrypoint/http/UserControllerTest.java
@@ -1,32 +1,22 @@
 package com.nssp.nottodo.entrypoint.http;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nssp.nottodo.core.usecase.dto.UserDto;
 import com.nssp.nottodo.core.usecase.inbound.IncludeUserInputInbound;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.HashMap;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
     @Autowired
     MockMvc mock;
-    @MockBean
+
+    @Autowired
     IncludeUserInputInbound include;
 
     @Test
@@ -42,8 +32,6 @@ public class UserControllerTest {
         dtoOutput.nick = "js";
         dtoOutput.id = 1L;
         dtoOutput.enabled = true;
-        when(include.includeUser(dtoInput)).thenReturn(dtoOutput);
-
         var payload = String.format("{\"name\":\"%s\",\"nick\":\"%s\",\"email\":\"%s\"}", dtoInput.name, dtoInput.nick, dtoInput.email);
          mock.perform(post("/v1/users")
         .contentType(MediaType.APPLICATION_JSON)
